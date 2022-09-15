87fca6c6939b6c61581931f84a785a28827f515e
RYE <93022160+RYE-513@users.noreply.github.com>
2022-09-15 22:24:35 +0800

    Update INCREMENT_DECREMENT.java

--- JAVA-MOMENT-main/JAVA 1/src/INCREMENT_DECREMENT.java
+++ JAVA-MOMENT-main/JAVA 1/src/INCREMENT_DECREMENT.java
@@ -15,6 +15,29 @@ public class INCREMENT_DECREMENT {
 			System.out.println(i);	  // this syntax shown like a while loop in negative numbers.
 		}
 
+		
+	// PRE-INCREMENT (BEFORE) "x++;"
+        // POST-INCREMENT (AFTER) "++x;"
+        
+        int x = 10;
+        
+        System.out.println(x++); // 10
+        System.out.println(++x); // 12 this post increment will add 1
+        
+        int xx = 10;
+        
+        System.out.println(++xx); // 11
+        System.out.println(xx++); // 11
+        
+        int y = 10;
+        
+        System.out.println(y--); // 10
+        System.out.println(--y); // 8 this post decrement will subtract 2
+        
+        int yy = 10;
+        
+        System.out.println(--yy); // 9
+        System.out.println(yy--); // 9
+		
 	}
-
 }
